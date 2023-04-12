b0d866491f749601edbd79966a4665349919a9c5
joaoabreu5 <joaomachadoabreu@hotmail.com>
2023-04-12 18:07:42 +0100

    Upload .gitignore

--- Sintaxe/parserExpr.py
+++ Sintaxe/parserExpr.py
@@ -3,7 +3,6 @@ import my_lexer
 
 tokens = my_lexer.tokens
 
-
 start = 'expr'
 
 def p_expr(p):
@@ -185,10 +184,6 @@ def p_exprList(p):
     else:
         p[0] = p[1] + p[3]
 
-
-    
-
-     
 
 parser = yacc.yacc(debug=True, write_tables=True)
 
